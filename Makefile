620e8acce3f58d0f604489fc936f7448c8a0ac83
Marcio <mllima@uolinc.com>
2020-03-27 16:51:05 -0300

    Busca de tweets da api do twiiter ignorando os retweets

--- Makefile
+++ Makefile
@@ -1,4 +1,11 @@
 #!make
+DOCKER_CMD:=docker run --rm -it -v "${PWD}":/app --env-file=.env -w /app golang:1.14-alpine
 
 vendor:
-	docker run --rm -it -v "${PWD}":/app -w /app golang:1.14-alpine go mod vendor
+	${DOCKER_CMD} go mod vendor
+
+run:
+	${DOCKER_CMD} go run .
+
+test:
+	${DOCKER_CMD} go test -v -race ./..
--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/url"
+	"os"
+
+	"github.com/ChimeraCoder/anaconda"
+)
 
 func main() {
-	fmt.Println("sai cachorro")
+	api := anaconda.NewTwitterApiWithCredentials(
+		os.Getenv("TWITTER_ACCESS_TOKEN"),
+		os.Getenv("TWITTER_ACCESS_SECRET"),
+		os.Getenv("TWITTER_CONSUMER_KEY"),
+		os.Getenv("TWITTER_SECRET_KEY"),
+	)
+
+	v := url.Values{}
+	v.Set("count", "10")
+	v.Set("lang", "pt")
+
+	searchResult, _ := api.GetSearch("#golang -filter:retweets", v)
+	for _, tweet := range searchResult.Statuses {
+		fmt.Println(tweet.Text, tweet.CreatedAt, tweet.Id)
+	}
 }
